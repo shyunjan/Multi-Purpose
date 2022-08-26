9ba1ba66ed49fcf8b3712cedab3c9b7e7eaf6c06
Shin Hyunjae <snhj3827@itechcompany.kr>
2022-08-26 15:05:51 +0900

    modified the user info of mongodb

--- src/config/local/config.db.js
+++ src/config/local/config.db.js
@@ -3,5 +3,5 @@ export default () => ({
   MONGO_DB_PORT: 27017,
   MONGO_DATABASE: 'multi-purpose',
   MONGO_DB_USER_NAME: 'appAdmin',
-  MONGO_DB_PASSWORD: 'ajf5gus&@wo',
+  MONGO_DB_PASSWORD: '^Ajf@2237!',
 });
